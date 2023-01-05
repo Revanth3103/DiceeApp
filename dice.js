8fc7689dcbf87d0d9fe0e724f242660ded66c10b
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-01-05 17:09:50 -0300

    Fixed images paths for the new workspace

--- dice.js
+++ dice.js
@@ -1,8 +1,8 @@
 function rollDice(){
    let randomizer1 = Math.floor(Math.random()*6)+1
    let randomizer2 = Math.floor(Math.random()*6)+1
-   document.querySelector(".img1").src = "/Dice Challenge/images/dice" + randomizer1 + ".png"
-   document.querySelector(".img2").src = "/Dice Challenge/images/dice" + randomizer2 + ".png"
+   document.querySelector(".img1").src = "/images/dice" + randomizer1 + ".png"
+   document.querySelector(".img2").src = "/images/dice" + randomizer2 + ".png"
    determineWinner(randomizer1, randomizer2)
 }
 
